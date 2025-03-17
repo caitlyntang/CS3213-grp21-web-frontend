5d67fe2870068f201422f8f3a8da476616a53556
zhenghuil <e0959119@u.nus.edu>
2025-03-17 16:38:38 +0800

    add GraphBox comp

---
+++ src/components/GraphBox.jsx
@@ -0,0 +1,31 @@
+import { Paper, Typography } from '@mui/material';
+import { LineChart } from '@mui/x-charts/LineChart';
+
+
+function GraphBox() {
+  const queries = 345677
+    return (
+      <Paper sx={{borderRadius:5, width:500, padding:2}} >
+        <Typography variant='h5' >
+          Total no.of queries
+        </Typography>
+        <Paper sx={{margin:1}}>
+          <Typography variant='h4' fontWeight={"bold"} textAlign='center'>
+            {queries}
+          </Typography>
+        </Paper>
+        <LineChart
+        xAxis={[{ data: [1, 2, 3, 5, 8, 10] }]}
+        series={[
+          {
+            data: [2, 5.5, 2, 8.5, 1.5, 5],
+          },
+        ]}
+        width={500}
+        height={300}
+        />
+      </Paper>
+      
+    )
+}
+export default GraphBox
--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
-import LineGraph from '../components/LineGraph';
 import StatBox from '../components/StatBox';
+import GraphBox from '../components/GraphBox';
 
 function Dashboard() {
   return (
     <div>
-      <h2>Dashboard</h2>
-      <p>Welcome to the Dashboard page!</p>
-      <LineGraph/>
+      <GraphBox/>
       <StatBox title="status: running" data='23423423'/>
+      
     </div>
   );
 }
