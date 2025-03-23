bd18f2ffa49211a7b34b7f71d3d3536dc784b50d
gnoossk <143413957+gnoossk@users.noreply.github.com>
2025-03-23 13:52:47 +0800

    edit dashboard for DB Logs

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import StatBox from '../components/StatBox';
 import GraphBox from '../components/GraphBox';
 import { Grid, Grid2 } from '@mui/material';
+import DatabaseLogger from '../components/DatabaseLogger';
 
 function Dashboard() {
   return (
@@ -24,6 +25,9 @@ function Dashboard() {
       </Grid2>
       <Grid2 size={3}>
         <StatBox title="Dbs/sec" data="2" />
+      </Grid2>
+      <Grid2 xs={12}>
+        <DatabaseLogger/>
       </Grid2>
     </Grid2>
   );
